bc4cc675a20831aadf44ad8691e2c147a85f3e9a
Cris Daniluk <cdaniluk@rhythmictech.com>
2022-02-01 11:01:01 -0500

    bring checks and other things up to current spec

---
+++ bin/install-ubuntu.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo 'installing dependencies'
+sudo apt install python3-pip gawk &&\
+pip3 install pre-commit
+
+# terraform docs
+mkdir tmp
+cd tmp
+curl -Lo ./terraform-docs.tar.gz https://github.com/terraform-docs/terraform-docs/releases/download/v0.16.0/terraform-docs-v0.16.0-$(uname)-amd64.tar.gz
+tar -xzf terraform-docs.tar.gz
+chmod +x terraform-docs
+sudo mv terraform-docs /usr/bin/
+cd ..
+rm -rf tmp
+
+curl -L "$(curl -sL https://api.github.com/repos/terraform-linters/tflint/releases/latest | grep -o -E "https://.+?_linux_amd64.zip")" > tflint.zip && unzip tflint.zip && rm tflint.zip && sudo mv tflint /usr/bin/
+env GO111MODULE=on go get -u github.com/liamg/tfsec/cmd/tfsec
+git clone https://github.com/tfutils/tfenv.git ~/.tfenv || true
+mkdir -p ~/.local/bin/
+. ~/.profile
+ln -s ~/.tfenv/bin/* ~/.local/bin
+
+echo 'installing pre-commit hooks'
+pre-commit install
+
+echo 'setting pre-commit hooks to auto-install on clone in the future'
+git config --global init.templateDir ~/.git-template
+pre-commit init-templatedir ~/.git-template
+
+echo 'installing terraform with tfenv'
+tfenv install
